09a6f928ecc465c7c0542358961d752bac84cf6c
Neeexy <nexy.2809@gmail.com>
2024-11-13 20:03:37 -0300

    <div class="get_name">

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
 // script.js
 const themeToggle = document.querySelector(".theme-checkbox")
+const SwapButton = document.querySelector('#SwapButton')
+const User_Name = document.querySelector('#User_Name')
+const get_nameDiv = document.querySelector('.get_name')
+const welcome_h1 = document.querySelector(".welcome")
+
+// Verifica se já existe um nome no LocalStorage ao carregar a página
+window.addEventListener('DOMContentLoaded', () =>{
+    const savedUser = localStorage.getItem('userName')
+    if(savedUser){
+        // Exibe a mensagem de boas-vindas com o nome salvo
+        welcome_h1.innerText = `Bem Vindo ${savedUser}!`
+        // Oculta a div get_name
+        get_nameDiv.style.display = 'none'
+    }
+})
+
+
+// Função que desliza a div (.get_name) para fora e salva o nome no localStorage
+
+function Swap_out(){
+    const user = User_Name.value.trim() // Remove os espaços em branco
+    if(user != 0){
+        // SALVA O NOME DO USUÁRIO NO LOCALSTORAGE
+        localStorage.setItem('userName', user)
+        // Exibe a mensagem de boas-vindas
+        welcome_h1.innerText = `Bem Vindo ${user}!`
+        // Oculta a div get_name
+        get_nameDiv.classList.add('swapped')
+    }else{
+        window.alert("Insira seu nome.")
+    }
+}
 
 themeToggle.addEventListener('change', function(){
     if(this.checked){
@@ -10,3 +42,4 @@ themeToggle.addEventListener('change', function(){
         document.body.classList.remove("dark-theme");
     }
 })
+
